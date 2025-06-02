371ce4f97af1798143aef251614e95dc7f92ddf7
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-06-02 19:13:24 +0000

    server

--- server.js
+++ server.js
@@ -4,26 +4,26 @@ const morgan = require('morgan');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-// Load environment variables
+m
 dotenv.config();
 
-// Connect to database
+
 connectDB();
 
-// Initialize Express app
+
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-// Sample route
+
 app.get('/', (req, res) => {
   res.send('API is running...');
   });
 
-  // Start server
+  
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
