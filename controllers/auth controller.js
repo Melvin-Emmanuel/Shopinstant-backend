bb4d16b44c344e66acaf64dbcd87d3b9cafff838
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-06-06 21:57:14 +0000

    modauthcontroller

--- controllers/auth controller.js
+++ controllers/auth controller.js
@@ -1,25 +1,21 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const generateToken = require('../utils/generateToken');
-
-// @desc    Register a new user
-// @route   POST /api/users/register
-// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
     const userExists = await User.findOne({ email });
 
-      if (userExists) {
+    if (userExists) {
           res.status(400);
-              throw new Error('User already exists');
-                }
+          throw new Error('User already exists');
+   }
 
-                  const user = await User.create({
-                      name,
-                          email,
-                              password,
-                                });
+    const user = await User.create({
+      name,
+      email,
+      password,
+  });
 
                                   if (user) {
                                       res.status(201).json({
