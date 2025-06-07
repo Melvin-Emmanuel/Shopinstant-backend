ab816391be868e8c05a27dfc072ec44e86139047
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-06-07 18:27:08 +0000

    modroutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, authUser } = require('../controllers/authController');
 
-// Register new user
 router.post('/register', registerUser);
-
-// Login user
 router.post('/login', authUser);
 
 module.exports = router;
