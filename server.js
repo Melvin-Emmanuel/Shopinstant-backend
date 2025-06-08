bbc9972a098f17d9645156ceffdb4a8d1f99763c
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-06-08 21:40:06 +0000

    newserver

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
+
+// Load environment variables
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+// Initialize Express app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
+app.use('/api/users', userRoutes);
+
+// Home route (optional)
+app.get('/', (req, res) => {
+  res.send('API is running...');
+  });
+
+  // Error Handling Middleware
+  app.use(notFound);
+  app.use(errorHandler);
+
+  // Start server
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    });
