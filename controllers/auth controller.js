9f7f48ca2b8d700909f62c9c2c80c8c4655c9e5d
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-06-11 20:46:40 +0000

    modauthnew

--- controllers/auth controller.js
+++ controllers/auth controller.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const generateToken = require('../utils/generateToken');
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+   const { name, email, password } = req.body;
 
     const userExists = await User.findOne({ email });
 
@@ -17,14 +17,14 @@ const registerUser = asyncHandler(async (req, res) => {
       password,
   });
 
-                                  if (user) {
-                                      res.status(201).json({
-                                            _id: user._id,
-                                                  name: user.name,
-                                                        email: user.email,
-                                                              isAdmin: user.isAdmin,
-                                                                    token: generateToken(user._id),
-                                                                        });
+    if (user) {
+       res.status(201).json({
+       _id: user._id,
+       name: user.name,              
+       email: user.email,
+       isAdmin: user.isAdmin,
+       token: generateToken(user._id),
+     });
                                                                           } else {
                                                                               res.status(400);
                                                                                   throw new Error('Invalid user data');
