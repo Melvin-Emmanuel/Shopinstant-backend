cb9605d28ebecfc1a9ab963b6e49e5b705064034
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-06-14 15:06:55 +0000

    newmodprodcontrol

--- controllers/product controller.js
+++ controllers/product controller.js
@@ -13,44 +13,36 @@ const getProducts = asyncHandler(async (req, res) => {
     // @route   GET /api/products/:id
     // @access  Public
     const getProductById = asyncHandler(async (req, res) => {
-      const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id);
 
-        if (product) {
-            res.json(product);
-              } else {
-                  res.status(404);
-                      throw new Error('Product not found');
-                        }
-                        });
+    if (product) {
+        res.json(product);
+  } else {
+     res.status(404);
+     throw new Error('Product not found');
+     }
+ });
+  const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
 
-                        // @desc    Delete a product
-                        // @route   DELETE /api/products/:id
-                        // @access  Private/Admin
-                        const deleteProduct = asyncHandler(async (req, res) => {
-                          const product = await Product.findById(req.params.id);
-
-                            if (product) {
-                                await product.remove();
-                                    res.json({ message: 'Product removed' });
-                                      } else {
-                                          res.status(404);
-                                              throw new Error('Product not found');
-                                                }
-                                                });
-
-                                                // @desc    Create a product
-                                                // @route   POST /api/products
-                                                // @access  Private/Admin
-                                                const createProduct = asyncHandler(async (req, res) => {
-                                                  const product = new Product({
-                                                      name: 'Sample name',
-                                                          price: 0,
-                                                              user: req.user._id,
-                                                                  image: '/images/sample.jpg',
-                                                                      brand: 'Sample brand',
-                                                                          category: 'Sample category',
-                                                                              countInStock: 0,
-                                                                                  numReviews: 0,
+  if (product) {
+     await product.remove();
+     res.json({ message: 'Product removed' });
+          } else {
+         res.status(404);
+         throw new Error('Product not found');
+                      }
+         });
+ const createProduct = asyncHandler(async (req, res) => {
+ const product = new Product({
+     name: 'Sample name',
+     price: 0,
+     user: req.user._id,
+     image: '/images/sample.jpg',
+     brand: 'Sample brand',
+     category: 'Sample category',
+     countInStock: 0,
+     numReviews: 0,
                                                                                       description: 'Sample description',
                                                                                         });
 
