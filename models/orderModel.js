ae4f3b343db0d9ac38bd71a81f2b1d815592944e
Melvin-Emmanuel <74471443+Melvin-Emmanuel@users.noreply.github.com>
2025-06-16 18:33:16 +0000

    modordermodel

--- models/orderModel.js
+++ models/orderModel.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 
 const orderItemSchema = new mongoose.Schema(
   {
-      name: { type: String, required: true },
-          qty: { type: Number, required: true },
-              image: { type: String, required: true },
-                  price: { type: Number, required: true },
-                      product: {
-                            type: mongoose.Schema.Types.ObjectId,
-                                  required: true,
-                                        ref: 'Product',
-                                            },
-                                              },
-                                                {
-                                                    timestamps: true,
-                                                      }
-                                                      );
+    name: { type: String, required: true },
+    qty: { type: Number, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    product: {
+              type: mongoose.Schema.Types.ObjectId,
+              required: true,
+              ref: 'Product',
+           },
+   },
+  {
+   timestamps: true,
+  }
+  );
 
                                                       const orderSchema = new mongoose.Schema(
                                                         {
